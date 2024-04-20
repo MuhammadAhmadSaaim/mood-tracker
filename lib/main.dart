cff8038c44d7e660701921ca081cf0a0f27614c3
MuhammadAhmadSaaim <ahmadsaaim2002@gmail.com>
2024-04-20 19:48:03 +0500

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
