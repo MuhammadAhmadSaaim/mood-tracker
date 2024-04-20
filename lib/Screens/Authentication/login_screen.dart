87ea5b71a5084dd9cfe3583f817f30edce628542
MuhammadAhmadSaaim <ahmadsaaim2002@gmail.com>
2024-04-20 20:13:08 +0500

    Fixed renderflow error

--- lib/Screens/Authentication/login_screen.dart
+++ lib/Screens/Authentication/login_screen.dart
@@ -8,8 +8,8 @@ class LoginPage extends StatelessWidget {
 
   LoginPage({super.key});
   //controllers
-  final usernameController=TextEditingController();
-  final passwordController=TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
   //for button
   void signinUser(){}
 
@@ -18,77 +18,78 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body:  SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 50,),
-              //logo
-              const SizedBox(height: 50,),
-              const Icon(
-                Icons.lock,
-                size: 100,
-              ),
-              const SizedBox(height: 50,),
-              //Text
-              Text(
-                "Welcome back you\ 've been missed!",
-                style: TextStyle(color: Colors.grey[700],fontSize: 16),
-              ),
-              const SizedBox(height: 25,),
-              //username
-              MyTextField(
-                contorller: usernameController,
-                hintText: "Username",
-                obscureText: false,
-              ),
-
-              const SizedBox(height: 15,),
-              //password
-              MyTextField(
-                contorller: passwordController,
-                hintText: "Password",
-                obscureText: true,
-              ),
-              const SizedBox(height: 10,),
-              //forget password
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 50,),
+                //logo
+                const SizedBox(height: 50,),
+                const Icon(
+                  Icons.lock,
+                  size: 100,
+                ),
+                const SizedBox(height: 50,),
+                //Text
+                Text(
+                  "Welcome back you\ 've been missed!",
+                  style: TextStyle(color: Colors.grey[700],fontSize: 16),
+                ),
+                const SizedBox(height: 25,),
+                //username
+                MyTextField(
+                  contorller: usernameController,
+                  hintText: "Username",
+                  obscureText: false,
+                ),
+          
+                const SizedBox(height: 15,),
+                //password
+                MyTextField(
+                  contorller: passwordController,
+                  hintText: "Password",
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10,),
+                //forget password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Forget Password?",
+                        style: TextStyle(color: Colors.grey[600]),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 25,),
+                //login button
+                MyButton(ontap: signinUser,),
+          
+                const SizedBox(height: 25,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      "Forget Password?",
-                      style: TextStyle(color: Colors.grey[600]),
+                      "Not a member?",
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    SizedBox(width: 4,),
+                    Text(
+                      "Register Now",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(height: 25,),
-              //login button
-              MyButton(ontap: signinUser,),
-
-              const SizedBox(height: 25,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Not a member?",
-                    style: TextStyle(
-                      color: Colors.grey[700],
-                    ),
-                  ),
-                  SizedBox(width: 4,),
-                  Text(
-                    "Register Now",
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Mood Tracker',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
