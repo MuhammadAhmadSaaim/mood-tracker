ebab5c87c7a5b815515cb9e7dd66ece52bdff716
MuhammadAhmadSaaim <ahmadsaaim2002@gmail.com>
2024-04-20 19:38:39 +0500

    Formatted Folder Structure

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mood Tracker',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
